f826dfa796b8166e3b5223f46020e35f4584b103
Levi Bohn <levibohn@gmail.com>
2023-02-22 04:57:18 -0600

    with direction from Chris Adams, I simply changed postID to postId, and things now work properly.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData);
 
-  const likePost = function (postId) {
+  const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -33,12 +33,12 @@ const App = () => {
         - otherwise just return the post object unchanged.
      */
     const updatedPosts = posts.map(post => {
-      if (post.id === postID) {
-        return { ...post, likes: post.likes + 1 };
+      if (post.id === postId) {
+        return { ...post, likes: post.likes + 1 }
       } else {
         return post;
       }
-    });
+    })
 
     setPosts(updatedPosts);
   };
